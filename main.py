bf742b845d4966d07279762d6e7198dfc77b6ffd
Justin <justinbhammett@gmail.com>
2024-10-20 18:24:51 -0700

    Fixed an error in main

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ def main():
 	print(f"Screen height: {SCREEN_HEIGHT}")
 	while True:
 		screen.fill((0,0,0))
+		pygame.display.flip()
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				return
 
 if __name__ == "__main__":
 	main()
